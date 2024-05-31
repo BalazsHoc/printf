07a549cc02385a2b96ee933522dbdcce881e6fd3
Balazs Hocsak <bhocsak@c3r5p4.42vienna.com>
2024-05-31 16:46:12 +0200

    unsigned long max and test_mix

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: bhocsak <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 10:46:11 by bhocsak           #+#    #+#             */
-/*   Updated: 2024/05/30 17:53:36 by bhocsak          ###   ########.fr       */
+/*   Updated: 2024/05/31 16:44:18 by bhocsak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,15 @@ int	ft_format(char *specifier, va_list ap)
 	else if (*specifier == 'p')
 		count += ft_putptr(va_arg(ap, void *));
 	else if (*specifier == 'd' || *specifier == 'i')
-		count += ft_putnbr_pf((long)va_arg(ap, int));
+		count += ft_putnbr_pf(va_arg(ap, int));
 	else if (*specifier == 'u')
 		count += ft_putuint(va_arg(ap, unsigned int));
 	else if (*specifier == 'x' || *specifier == 'X')
 	{
 		if (*specifier == 'x')
-			count += ft_puthex((long)va_arg(ap, unsigned long), 'x');
+			count += ft_puthex(va_arg(ap, unsigned int), 'x');
 		else
-			count += ft_puthex((long)va_arg(ap, unsigned long), 'X');
+			count += ft_puthex(va_arg(ap, unsigned int), 'X');
 	}
 	else
 		count += write(1, specifier, 1);
@@ -69,11 +69,16 @@ int	ft_printf(const char *format, ...)
 int	main()
 {
 	int	count = 0;
-	int og_count = 0;
+	int count2 = 0;
+	int	orig = 0;
+	int	orig2 = 0;;
 //	long hex = 454147483649;
 
-	count += ft_printf("\n");
-	ft_printf("Myprintf: %i\n", count);
-	og_count += printf("\n");
-	printf("Ogprintf: %i\n", og_count);
+	count += ft_printf("%x\n", ULONG_MAX);
+	orig += printf("%lx\n", ULONG_MAX); //doesnt matter
+	ft_printf("Myprintf ulongmax: %i\n", count);
+	printf("Origprintf ulongmax: %i\n", orig);
+	count2 += ft_printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%\n", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+	orig2 += printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%\n", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+	printf("Orig_printf testmix: %i\n", orig2);
 }*/
